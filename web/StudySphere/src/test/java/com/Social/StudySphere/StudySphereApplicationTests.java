0faa1fae85c8176be16216f935a6bc09ee846064
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-09-24 11:44:44 -0400

    web setup

---
+++ web/StudySphere/src/test/java/com/Social/StudySphere/StudySphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Social.StudySphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
